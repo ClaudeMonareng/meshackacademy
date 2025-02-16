b7a9f0b79f23a2ac197b183d32c1e04e6ca314ac
Lehlohonolo Monareng <43777245+ClaudeMonareng@users.noreply.github.com>
2025-02-16 18:51:38 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,52 +1,68 @@
-        // TOGGLE NAVBAR
 
-        // const menuBtn = document.querySelector(".nav-menu-btn");
-        // const closeBtn = document.querySelector(".nav-close-btn");
-        // const navigationBtn = document.querySelector(".navigation");
+// TOGGLE NAVBAR
 
-        // menuBtn.addEventListener("click", () => {
-        //     menuBtn.classList.toggle("active");
-        //     navigationBtn.classList.toggle("active");
-        // });
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
 
-        // TOGGLE NAVBAR
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
 
-        const menuBtn = document.querySelector(".menu-btn");
-        const navigation = document.querySelector(".navigation");
+// now, we will fix navigation issue...menu close after clicked any link
 
-        menuBtn.addEventListener("click", () => {
-            menuBtn.classList.toggle("active");
-            navigation.classList.toggle("active");
+function toggleMenu() {
+    menuBtn.classList.remove('active');
+    navigation.classList.remove('active');
+}
+
+// STICKY NAVBAR
+// NAVIGATION BAR EFFECTS ON SCROLL
+
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// INITIALIZE SWIPER
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 8000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+// PORTFOLIO SECTION - MODAL
+
+const portfolioModals = document.querySelectorAll(".portfolio-modal");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns= document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function(modalClick){
+    portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) => {
+    imgCard.addEventListener("click", () => {
+        portfolioModal(i);
+    });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+    portfolioCloseBtn.addEventListener("click", () => {
+    portfolioModals.forEach((portfolioModalView) => {
+            portfolioModalView.classList.remove("active");
         });
-
-        // STICKY NAVBAR
-        // NAVIGATION BAR EFFECTS ON SCROLL
-
-        window.addEventListener("scroll", function () {
-            const header = document.querySelector("header");
-            header.classList.toggle("sticky", window.scrollY > 0);
-        });
-
-        // PORTFOLIO SECTION - MODAL
-
-        const portfolioModals = document.querySelectorAll(".portfolio-modal");
-        const imgCards = document.querySelectorAll(".img-card");
-        const portfolioCloseBtns= document.querySelectorAll(".portfolio-close-btn");
-
-        var portfolioModal = function(modalClick){
-            portfolioModals[modalClick].classList.add("active");
-        }
-        
-        imgCards.forEach((imgCard, i) => {
-            imgCard.addEventListener("click", () => {
-                portfolioModal(i);
-            });
-        });
-
-        portfolioCloseBtns.forEach((portfolioCloseBtn) => {
-            portfolioCloseBtn.addEventListener("click", () => {
-            portfolioModals.forEach((portfolioModalView) => {
-                    portfolioModalView.classList.remove("active");
-                });
-            });
-        });
+    });
+});
