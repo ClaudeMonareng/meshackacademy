46214eb3a2967cbb1bf7b4f3e1c1aed6c02e4246
Lehlohonolo Monareng <43777245+ClaudeMonareng@users.noreply.github.com>
2026-02-21 01:00:31 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 
 // TOGGLE NAVBAR
 
-const menuBtn = document.querySelector(".menu-btn");
-const navigation = document.querySelector(".navigation");
+  const menuBtn = document.querySelector(".nav-menu-btn");
+  const closeBtn = document.querySelector(".nav-close-btn");
+  const navigation = document.querySelector(".navigation");
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
     navigation.classList.toggle("active");
 });
+
+closeBtn.addEventListener("click", () => {
+
+    navigation.classList.remove("active");
+})
 
 // now, we will fix navigation issue...menu close after clicked any link
 
@@ -16,53 +22,10 @@ function toggleMenu() {
     navigation.classList.remove('active');
 }
 
-// STICKY NAVBAR
-// NAVIGATION BAR EFFECTS ON SCROLL
+// STICKY NAVBAR                            
+// NAVIGATION BAR EFFECTS ON SCROLL 
 
 window.addEventListener("scroll", function () {
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
-});
-
-// INITIALIZE SWIPER
-
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 8000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-});
-
-// PORTFOLIO SECTION - MODAL
-
-const portfolioModals = document.querySelectorAll(".portfolio-modal");
-const imgCards = document.querySelectorAll(".img-card");
-const portfolioCloseBtns= document.querySelectorAll(".portfolio-close-btn");
-
-var portfolioModal = function(modalClick){
-    portfolioModals[modalClick].classList.add("active");
-}
-
-imgCards.forEach((imgCard, i) => {
-    imgCard.addEventListener("click", () => {
-        portfolioModal(i);
-    });
-});
-
-portfolioCloseBtns.forEach((portfolioCloseBtn) => {
-    portfolioCloseBtn.addEventListener("click", () => {
-    portfolioModals.forEach((portfolioModalView) => {
-            portfolioModalView.classList.remove("active");
-        });
-    });
 });
